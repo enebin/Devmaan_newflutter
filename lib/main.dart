3bd57b91e6e617f953880c25632c92b087caee5e
enebin <lyb35548@gmail.com>
2021-12-16 16:39:21 +0900

    xc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'dart:io';
-import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'util.dart';
+import 'model/notice.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,83 +33,189 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   bool isWeb = false;
+  List<Notice> notices = [
+    Notice(
+      company: Company(name: "Naver"),
+      title: "귀여운 뉴비 모바일 개발자 모집",
+      date: "2021-2022",
+      link: "www.naver.com",
+      year: Year(year: 0),
+    )
+  ];
 
   @override
   void initState() {
     super.initState();
-    checkIsWeb();
-  }
-
-  void checkIsWeb() {
-    try {
-      if (Platform.isAndroid || Platform.isIOS) {
-        isWeb = false;
-      } else {
-        isWeb = true;
-      }
-    } catch (e) {
-      isWeb = true;
-    }
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    Util util = Util();
+    isWeb = util.checkIsWeb();
+    util.fetchData(notices);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Banner(),
+          CompanyList(),
+          NoticeItem(notices: notices),
+          Spacer(),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Stack(children: [
-              Container(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    'assets/background.jpg',
-                    width: double.infinity,
-                    height: 200,
-                    fit: BoxFit.fill,
-                  )),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  Text("Dev만 다 있음!",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white))
+    );
+  }
+}
+
+class NoticeItem extends StatefulWidget {
+  const NoticeItem({
+    Key? key,
+    required this.notices,
+  }) : super(key: key);
+
+  final List<Notice> notices;
+
+  @override
+  State<NoticeItem> createState() => _NoticeItemState();
+}
+
+class _NoticeItemState extends State<NoticeItem> {
+  double scale = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 200),
+          height: 100 * scale,
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Center(
+              child: Column(
+                children: [
+                  Text(widget.notices[0].title),
+                  SizedBox(height: 15),
+                  Image.asset(
+                    'assets/' +
+                        widget.notices[0].company.getName().toLowerCase() +
+                        '.png',
+                    width: 70,
+                    fit: BoxFit.fitWidth,
+                  )
                 ],
               ),
-            ]),
-            GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
-                childAspectRatio: 1 / 2, //item 의 가로 1, 세로 2 의 비율
-                mainAxisSpacing: 10, //수평 Padding
-                crossAxisSpacing: 10, //수직 Padding
-              ),
-              itemCount: ,
             ),
-            Spacer(),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      onTap: () {
+        print("SSS");
+      },
+      onHover: (isHovering) {
+        if (isHovering) {
+          print("DD!!");
+          setState(() {
+            scale = 1.3;
+          });
+        } else {
+          setState(() {
+            scale = 1.0;
+          });
+        }
+      },
+    );
+  }
+}
+
+class CompanyList extends StatelessWidget {
+  final List<String> _iconAssets = [
+    'assets/coupang.png',
+    'assets/kakao.png',
+    'assets/line.png',
+    'assets/naver.png'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 5,
+              offset: Offset(0, 3))
+        ],
       ),
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: _iconAssets.length,
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        itemBuilder: (BuildContext context, int index) {
+          return Row(
+            children: [
+              Image.asset(
+                _iconAssets[index],
+                width: 80,
+                fit: BoxFit.fitWidth,
+              ),
+              SizedBox(width: 20)
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Banner extends StatelessWidget {
+  const Banner({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Stack(fit: StackFit.loose, children: [
+        Positioned.fill(
+          child: Align(
+            alignment: Alignment.center,
+            child: Image.asset(
+              'assets/background.jpg',
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        Positioned.fill(
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Top 7 채용 공고는\n 여기 다 모여있음!",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
---
+++ lib/util.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'package:firebase_database/firebase_database.dart';
+import 'model/notice.dart';
+
+class Util {
+  bool checkIsWeb() {
+    bool isWeb;
+    try {
+      if (Platform.isAndroid || Platform.isIOS) {
+        isWeb = false;
+      } else {
+        isWeb = true;
+      }
+    } catch (e) {
+      isWeb = true;
+    }
+
+    return isWeb;
+  }
+
+  void fetchData(List<Notice> _notices) {
+    final databaseReference = FirebaseDatabase.instance.reference();
+
+    databaseReference.child('notices').once().then((DataSnapshot snapshot) {
+      final Map<String, dynamic> data =
+          Map<String, dynamic>.from(snapshot.value);
+
+      _notices = List<Notice>.from(
+          data.values.map((notice) => Notice.fromJson(notice)));
+      print("[System] ${_notices.length} notices fetched.");
+    });
+  }
+}
